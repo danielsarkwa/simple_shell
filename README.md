2bc239fe83d6236ef0c312acc2524f902551b5fa
princesszion <maikemvictorine@gmail.com>
2021-08-26 14:19:15 +0200

    new

--- README.md
+++ README.md
@@ -1,11 +1,45 @@
-# simple_shell
-A simple UNIX command interpreter
+<h1 style="text-align: center">Simple Shell</h1>
+Here's a screenshot of our simple shell in action.
 
-This repository contains the files for alx-Holberton's simple_shell project. It can be compiled using GCC and will execute a simple shell that can be used for some basic tasks and programs most commonly found in the /bin/ folder.
+<img src="./readme/img-0.png"/>
 
-GCC command to compile:
-$| gcc -Wall -Werror -Wextra -pedantic *.c -o BAShell
-This wil compile all the '.c' files and change the output's name to 'BAShell'.
+## How To Use
 
-Features
-BAShell is a simple shell made in C to interact with a linux operating system.
+Run the command below to build the shell.
+
+```powershell
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c -o simple_shell
+```
+
+After compiling the program, run `./simple_shell` to startup the shell in interactive mode. To start the shell in non-interactive mode, run `./simple_shell file`, where `file` is a text-encoded file containing lines of commands to be executed. You can also pipe a string of commands to the program like `echo 'echo $PWD' | ./simple_shell` to be run in a non-interactive mode.
+
+## Supported Control Operators
+
+| Operator | Description |
+|:--:|:--|
+| ; | Separates commands into command lists that can be executed unconditionally (doesn't depend on the failure or success of the previous command) |
+| && | Executes the next command in a command list if the previous command succeeded (had an exit code of 0) |
+| \|\| | Executes the next command in a command list if the previous command failed (had a non-zero exit code) |
+
+## Supported Built-In Commands
+
+1. **Command:** *alias*<br/>**Usage:** `alias [name[='value'] ...]`<br/>**Description:** Prints a list of aliases when no arguments are provided. If a name is provided in the arguments, it prints the alias' value in the form `name='value'`. If a name-value pair is provided, it creates or assigns an alias (*name*) with the value *value*. This only happens in the *interactive* mode.
+2. **Command:** *cd*<br/>**Usage:** `cd [DIRECTORY]`<br/>**Description:** Changes the shell's current working directory to *DIRECTORY* if it is provided. If *DIRECTORY* is *-*, it switches the current working directory (*PWD*) with the previous working directory (*OLDPWD*). If *DIRECTORY* is not provided, the current working directory is changed to the home directory.
+3. **Command:** *env*<br/>**Usage:** `env`<br/>**Description:** Prints the list of environment variables.
+4. **Command:** *exit*<br/>**Usage:** `exit status`<br/>**Description:** Exits the shell with status code *status* (ranges from 0 to 255 inclusive) or the status code of the last command that was executed.
+5. **Command:** *help*<br/>**Usage:** `help`<br/>**Description:** Prints the help page of a built-in command or the program.
+6. **Command:** *history*<br/>**Usage:** `history`<br/>**Description:** Prints the list of commands that have been stored by the shell.
+7. **Command:** *setenv*<br/>**Usage:** `setenv VARIABLE VALUE`<br/>**Description:** Sets the value of an environment variable named *VARIABLE* to *VALUE*.
+8. **Command:** *unsetenv*<br/>**Usage:** `unsetenv VARIABLE`<br/>**Description:** Removes an environment variable named *VARIABLE*.
+
+## Getting Started (for contributors)
+
+Install GCC on your system.
+Load the bash script into your BASH terminal using `source .bashrc`. This would enable the following functions to be used:
+
+1. `build` - Builds the program
+2. `build_dbg` - Creates a debugging build of the program
+3. `run` - Runs the built version of the program
+4. `install` - Installs the built version of the program
+5. `val` - Launches the program with **Valgrind** (should be used after **build_dbg**)
+6. `viewman` - Opens the man page for the program
--- parser.c
+++ parser.c
@@ -1,76 +1,172 @@
-#include "shell.h"
-/**
- * is_path_form - chekc if the given fikenname is a path
- * @data: the data strucct pointer
- *
- * Return: (Success)
- * ------- (Fail) otherwise
- */
-int is_path_form(sh_t *data)
-{
-if (_strchr(data->args[0], '/') != 0)
-{
-data->cmd = _strdup(data->args[0]);
-return (SUCCESS);
-}
-return (FAIL);
-}
-#define DELIMITER ":"
-/**
- * is_short_form - chekc if the given fikenname is short form
- * @data: the data strucct pointer
- *
- * Return: (Success)
- * ------- (Fail) otherwise
- */
-void is_short_form(sh_t *data)
-{
-char *path, *token, *_path;
-struct stat st;
-int exist_flag = 0;
+#include "main.h"
 
-path = _getenv("PATH");
-_path = _strdup(path);
-token = strtok(_path, DELIMITER);
-while (token)
-{
-data->cmd = _strcat(token, data->args[0]);
-if (stat(data->cmd, &st) == 0)
-{
-exist_flag += 1;
-break;
-}
-free(data->cmd);
-token = strtok(NULL, DELIMITER);
-}
-if (exist_flag == 0)
-{
-data->cmd = _strdup(data->args[0]);
-}
-free(_path);
-}
-#undef DELIMITER
 /**
- * is_builtin - checks if the command is builtin
- * @data: a pointer to the data structure
+ * parse_cmd_line - Processes a line of commands into a list of cmd_list_t
+ * @line: The line of commands
  *
- * Return: (Success) 0 is returned
- * ------- (Fail) negative number will returned
+ * Return: The list of cmd_list_t
  */
-int is_builtin(sh_t *data)
+cmd_t *parse_cmd_line(char *line)
 {
-blt_t blt[] = {
-{"exit", abort_prg},
-{"cd", change_dir},
-{"help", display_help},
-{NULL, NULL}
-};
-int i = 0;
-while ((blt + i)->cmd)
-{
-if (_strcmp(data->args[0], (blt + i)->cmd) == 0)
-return (SUCCESS);
-i++;
+	proc_tbl_t *proc_tbl;
+	token_t *tokens = tokenize_command_string(line);
+	char **alias_expansions = NULL;
+	int n = 0;
+	cmd_t *cmd_list = NULL;
+
+	tokens = process_alias_expansion(&tokens);
+	if (tokens == NULL)
+		return (NULL);
+	proc_tbl = malloc(sizeof(proc_tbl_t));
+	if (proc_tbl == NULL)
+		return (NULL);
+	init_processing_table(&proc_tbl, &tokens, &cmd_list);
+	while ((proc_tbl->cur_token != NULL) && (proc_tbl->error == FALSE))
+	{
+		if ((proc_tbl->cur_token)->type == TKN_SEP_OP)
+			process_commands_separator(proc_tbl);
+		else if ((proc_tbl->cur_token)->type == TKN_OP)
+			process_operator(proc_tbl);
+		else if ((proc_tbl->cur_token)->type == TKN_WORD)
+			process_word(proc_tbl);
+		proc_tbl->pos++;
+		proc_tbl->cur_token = (proc_tbl->cur_token)->next;
+	}
+	free_token_t(&tokens);
+	if (proc_tbl->error == TRUE)
+	{
+		free_cmd_t(&cmd_list);
+		*((uchar_t *)get_shell_prop(NODE_EXIT_CODE_ID)) = EC_INVALID_ARGS;
+	}
+	if (proc_tbl != NULL)
+		free(proc_tbl);
+	free_array(alias_expansions, n);
+	return (cmd_list);
 }
-return (NEUTRAL);
+
+/**
+ * init_processing_table - Initializes the processing table
+ * @proc_tbl: The pointer to the processing table
+ * @tokens: The pointer to the tokens to be processed
+ * @cmd_list: The pointer to the command list's head
+ */
+void init_processing_table(proc_tbl_t **proc_tbl,
+	token_t **tokens, cmd_t **cmd_list)
+{
+	(*proc_tbl)->error = FALSE;
+	(*proc_tbl)->tokens_list = tokens;
+	(*proc_tbl)->cmds_list_head = cmd_list;
+	(*proc_tbl)->cur_cmd_node = NULL;
+	(*proc_tbl)->pos = 0;
+	(*proc_tbl)->cur_token = *tokens;
+}
+
+/**
+ * process_commands_separator - Processes a separator
+ * @proc_tbl: The pointer to the processing table
+ */
+void process_commands_separator(proc_tbl_t *proc_tbl)
+{
+	char *prog_name = *((char **)get_shell_prop(EXEC_NAME_ID));
+	char *line_num = NULL;
+	token_t *tmp_tkn = NULL;
+
+	tmp_tkn = get_token_at_index(proc_tbl->pos - 1, proc_tbl->tokens_list);
+	if ((tmp_tkn == NULL) || (tmp_tkn->type != TKN_WORD))
+	{
+		line_num = long_to_str(get_line_num());
+		write(STDERR_FILENO, prog_name, str_len(prog_name));
+		write(STDERR_FILENO, ": ", 2);
+		if (line_num != NULL)
+			write(STDERR_FILENO, line_num, str_len(line_num));
+		write(STDERR_FILENO, ": Syntax error: \"", 17);
+		write(STDERR_FILENO, proc_tbl->cur_token->value,
+			str_len(proc_tbl->cur_token->value));
+		write(STDERR_FILENO, "\" unexpected\n", 13);
+		if (line_num != NULL)
+			free(line_num);
+		proc_tbl->error = TRUE;
+	}
+	else
+	{
+		if (proc_tbl->cur_cmd_node != NULL)
+			(proc_tbl->cur_cmd_node)->next_cond = OP_SEP;
+	}
+}
+
+/**
+ * process_operator - Processes an separator
+ * @proc_tbl: The pointer to the processing table
+ */
+void process_operator(proc_tbl_t *proc_tbl)
+{
+	char *prog_name = *((char **)get_shell_prop(EXEC_NAME_ID));
+	char *line_num = NULL;
+	token_t *tmp_tkn = NULL;
+
+	tmp_tkn = get_token_at_index(proc_tbl->pos - 1, proc_tbl->tokens_list);
+	if ((tmp_tkn == NULL) || (tmp_tkn->type != TKN_WORD))
+	{
+		line_num = long_to_str(get_line_num());
+		write(STDERR_FILENO, prog_name, str_len(prog_name));
+		write(STDERR_FILENO, ": ", 2);
+		if (line_num != NULL)
+			write(STDERR_FILENO, line_num, str_len(line_num));
+		write(STDERR_FILENO, ": Syntax error: \"", 17);
+		write(STDERR_FILENO, proc_tbl->cur_token->value,
+			str_len(proc_tbl->cur_token->value));
+		write(STDERR_FILENO, "\" unexpected\n", 13);
+		if (line_num != NULL)
+			free(line_num);
+		proc_tbl->error = TRUE;
+	}
+	else
+	{
+		if (proc_tbl->cur_cmd_node != NULL)
+		{
+			if (str_eql(proc_tbl->cur_token->value, "&&"))
+				(proc_tbl->cur_cmd_node)->next_cond = OP_AND;
+			else if (str_eql(proc_tbl->cur_token->value, "||"))
+				(proc_tbl->cur_cmd_node)->next_cond = OP_OR;
+		}
+	}
+}
+
+/**
+ * process_word - Processes a word
+ * @proc_tbl: The pointer to the processing table
+ */
+void process_word(proc_tbl_t *proc_tbl)
+{
+	cmd_t *node = NULL;
+	token_t *tmp_tkn = NULL;
+
+	tmp_tkn = get_token_at_index(proc_tbl->pos - 1, proc_tbl->tokens_list);
+	if ((tmp_tkn == NULL) || (tmp_tkn->type != TKN_WORD))
+	{
+		/* create node to be added as a command */
+		node = new_cmd_node();
+		if (node != NULL)
+		{
+			node->command = str_copy(proc_tbl->cur_token->value);
+			add_node_to_end(proc_tbl->cmds_list_head, &node);
+			proc_tbl->cur_cmd_node = node;
+		}
+	}
+	else
+	{
+		/* add argument to current command */
+		if (proc_tbl->cur_cmd_node != NULL)
+		{
+			(proc_tbl->cur_cmd_node)->args = _realloc(
+				(proc_tbl->cur_cmd_node)->args,
+				sizeof(void *) * ((proc_tbl->cur_cmd_node)->args_count),
+				sizeof(void *) * ((proc_tbl->cur_cmd_node)->args_count + 1)
+				);
+			*((proc_tbl->cur_cmd_node)->args
+				+ (proc_tbl->cur_cmd_node)->args_count) = str_copy(
+					proc_tbl->cur_token->value);
+			(proc_tbl->cur_cmd_node)->args_count++;
+		}
+	}
 }
